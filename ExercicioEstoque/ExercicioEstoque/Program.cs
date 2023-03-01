0e2adbc07568be73003cf308fdf7ca7ca7e208c8
MatheusRodriguesSilva <matheusmtt2323@gmail.com>
2023-03-01 13:03:06 -0300

    ExerciciosModulo2

---
+++ ExercicioEstoque/ExercicioEstoque/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // teste mercadoria
+        List<Mercadoria> mercadorias = new List<Mercadoria>();
+
+        // cadastro de mercadorias
+        Mercadoria mercadoria1 = new Mercadoria("Camisa", 50.0, 100);
+        Mercadoria mercadoria2 = new Mercadoria("Calça", 100.0, 50);
+        mercadorias.Add(mercadoria1);
+        mercadorias.Add(mercadoria2);
+
+        // busca de mercadoria pelo nome
+        string nomeBusca = "Camisa";
+        Mercadoria mercadoriaBusca = mercadorias.Find(mercadoria => mercadoria.Nome == nomeBusca);
+        if (mercadoriaBusca != null)
+        {
+            Console.WriteLine("Mercadoria encontrada: " + mercadoriaBusca.Nome);
+        }
+        else
+        {
+            Console.WriteLine("Mercadoria não encontrada");
+        }
+
+        // adição de estoque de mercadoria
+        int quantidadeAdicao = 50;
+        mercadoria1.AdicionarEstoque(quantidadeAdicao);
+        Console.WriteLine("Quantidade de " + mercadoria1.Nome + " após adição de estoque: " + mercadoria1.Quantidade);
+    }
+
+
+    class Mercadoria
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Mercadoria(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public void AdicionarEstoque(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverEstoque(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public class Cliente
+        {
+            public string Nome { get; set; }
+            public string Endereco { get; set; }
+            public string Telefone { get; set; }
+        }
+
+        // Classe principal para o controle de estoque
+        public class ControleEstoque
+        {
+            // Lista de clientes cadastrados
+            private List<Cliente> clientes;
+
+            public ControleEstoque()
+            {
+                clientes = new List<Cliente>();
+            }
+
+            // Método para cadastrar um novo cliente
+            public void CadastrarCliente(string nome, string endereco, string telefone)
+            {
+                Cliente novoCliente = new Cliente();
+                novoCliente.Nome = nome;
+                novoCliente.Endereco = endereco;
+                novoCliente.Telefone = telefone;
+
+                clientes.Add(novoCliente);
+            }
+        }
+    }
+}
---
+++ ExercicioProfessor/ExerciciosModulo2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Pessoa
+{
+    public string nome;
+    public int idade;
+
+    public void Cumprimentar()
+    {
+        Console.WriteLine($"Olá, meu nome é {nome}!");
+    }
+
+    public void DizerIdade()
+    {
+        Console.WriteLine($"Eu tenho {idade} anos.");
+    }
+}
+
+class Aluno : Pessoa
+{
+    public void IrParaEscola()
+    {
+        Console.WriteLine($"estou indo para a escola .");
+    }
+}
+
+class Professor : Pessoa
+{
+    public void Explicar(string assunto)
+    {
+        Console.WriteLine($"{nome} está explicando sobre {assunto}.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "João";
+        pessoa.idade = 15;
+        pessoa.Cumprimentar();
+        pessoa.DizerIdade();
+
+        Aluno aluno = new Aluno();
+        aluno.nome = "Maria";
+        aluno.idade = 10;
+        aluno.Cumprimentar();
+        aluno.DizerIdade();
+        aluno.IrParaEscola();
+
+        Professor professor = new Professor();
+        professor.nome = "Pedro";
+        professor.idade = 40;
+        professor.Cumprimentar();
+        professor.DizerIdade();
+        professor.Explicar("matemática");
+    }
+}
